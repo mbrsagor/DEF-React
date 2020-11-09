7c7afb8d2d0ffe046fa0a4a6251b7fcfa5528762
Sagor <mbrsagor@gmail.com>
2020-11-10 02:14:36 +0600

    Refactor repo

--- rferaApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RferaappConfig(AppConfig):
-    name = 'rferaApp'
--- rferaApp/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-from .views import DeviceViewSet
-
-router.register('device', DeviceViewSet)
-
-urlpatterns = router.urls
